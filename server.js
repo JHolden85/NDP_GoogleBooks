5a451adfb978a1259f1bf71c42b740c12367ece6
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-01 10:00:18 -0800

    Added paths to the server and moved the .git to the Main level

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes")
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Define middleware here - body parsing for AJAZ requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define routes
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
